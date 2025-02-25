b85a24b8abc7e884b5218ed086449cf308eae497
andre-kumagae <andre.kumagae@hotmail.com>
2025-02-25 16:10:27 -0300

    Aula 256 - JDBC pt 05 - Adicionando dependência e conectando com banco

--- youtube/dev-dojo/maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectionFactory.java
+++ youtube/dev-dojo/maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectionFactory.java
@@ -1,8 +1,23 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.conn;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
     //java.sql = pacote para conectar Java com SQL
     //Os devs de Java criaram Interfaces como padrão = Connection, Statement, ResulSet, DriverManager
     // as empresas como MySQL, Postgres, MariaDB, adaptassem a conexao a elas
-
+    public static Connection getConnection() {
+        String url =  "jdbc:mysql://localhost:3306/anime_store";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
