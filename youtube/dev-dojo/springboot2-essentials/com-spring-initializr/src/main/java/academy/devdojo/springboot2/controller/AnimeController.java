a618f97c695304b33fd1cdb0ff87f19f4d6ec4c4
andre-kumagae <andre.kumagae@hotmail.com>
2025-03-04 20:24:42 -0300

    Spring Boot 2 Essentials 07 - Método GET parte 1

--- youtube/dev-dojo/springboot2-essentials/com-spring-initializr/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ youtube/dev-dojo/springboot2-essentials/com-spring-initializr/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -11,25 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
-// anotacao que retorna String via ResponseBody para o valor Json
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
-//@AllArgsConstructor
 @RequiredArgsConstructor
 public class AnimeController {
     private final DateUtil dateUtil;
-//    @Autowired
-
-
-//    public AnimeController(DateUtil dateUtil) {
-//        this.dateUtil = dateUtil;
-//    }
+    private final AnimeService animeService;
 
     //localhost:8080/anime/list
-    @GetMapping(path = "list")
+    @GetMapping
     public List<Anime> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"),new Anime("Boku no Hero"), new Anime("Berserk"), new Anime("One Piece"), new Anime("Naruto"));
+        return animeService.listAll();
     }
 }
