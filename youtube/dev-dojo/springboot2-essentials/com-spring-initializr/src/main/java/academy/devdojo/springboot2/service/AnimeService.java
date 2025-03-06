3cf53c03b99b4f03c9d1d63e3e79f2546dbc8f47
andre-kumagae <andre.kumagae@hotmail.com>
2025-03-06 16:01:46 -0300

    Spring Boot 2 Essentials 19 - Transações

--- youtube/dev-dojo/springboot2-essentials/com-spring-initializr/src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ youtube/dev-dojo/springboot2-essentials/com-spring-initializr/src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -6,10 +6,9 @@ import academy.devdojo.springboot2.mapper.AnimeMapper;
 import academy.devdojo.springboot2.repository.AnimeRepository;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -31,6 +30,7 @@ public class AnimeService {
                 .orElseThrow(() -> new BadRequestException("Anime not found"));
     }
 
+    @Transactional
     public Anime save(AnimePostRequestBody animePostRequestBody) {
         return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
     }
