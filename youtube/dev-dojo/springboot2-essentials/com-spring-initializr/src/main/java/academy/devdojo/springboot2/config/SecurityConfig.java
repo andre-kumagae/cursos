5ae89f2f3f8fdcb5c0b0831686b22169d7acec2b
andre-kumagae <andre.kumagae@hotmail.com>
2025-03-14 10:10:13 -0300

    Spring Boot 2 Essentials 41 - Spring Security pt 02 - Segurança a nível de métodos com PreAuthoritorize

--- youtube/dev-dojo/springboot2-essentials/com-spring-initializr/src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ youtube/dev-dojo/springboot2-essentials/com-spring-initializr/src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
